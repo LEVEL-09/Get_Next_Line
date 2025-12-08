d7cf38baf70f0a2383c0e7a3fe33dbc0034eb04e
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-12-08 13:55:36 +0100

    test read function

--- main.c
+++ main.c
@@ -5,5 +5,13 @@
 
 int main()
 {
-	int fd = open("text.txt", 1, O_RDONLY);
+	int fd = open("text.txt", O_RDONLY);
+	char buf[11];
+	int i;
+
+	read(fd, buf, (size_t)10);
+
+	printf("%s \n", buf);
+
+	close(fd);
 }
