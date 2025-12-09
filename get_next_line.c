8d2f46956b7e37cb80ca24354e4c27f8531f0eab
Leon <hichamohicham651@gmail.com>
2025-12-09 17:02:22 +0100

    fix sigfult and read one line :)

--- get_next_line.c
+++ get_next_line.c
@@ -12,22 +12,42 @@
 
 #include "get_next_line.h"
 
-#include <stdio.h>
+static void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	if (dest == src)
+		return (dest);
+	ptr = (unsigned char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		*(ptr + i) = *((unsigned char *)src + i);
+		i++;
+	}
+	return (dest);
+}
+
 char	*get_next_line(int fd)
 {
 	static size_t	i;
 	char			*buf;
+	char 			*line;
 
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	while (1)
+	read(fd, buf, BUFFER_SIZE);
+	while (i < BUFFER_SIZE)
 	{
-		read(fd, buf, i+1);
-		printf("%zu \n", i);
 		if (buf[i++] == '\n')
 			break;
 	}
-	
-	return (buf);
+
+	line = malloc(sizeof(char) * i);
+	if (!line)
+		return (line);
+	ft_memcpy(line, buf, i);
+	return (line);
 }
