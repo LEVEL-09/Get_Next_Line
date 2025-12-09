f52ea3dbecb943fb448cd56cf2a4dd382c728113
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-12-09 11:22:44 +0100

    Define buffer size macro

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 09:20:26 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/08 09:20:27 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/12/09 10:58:35 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 char *get_next_line(int fd)
 {
+	char	*buf;
 
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 }
