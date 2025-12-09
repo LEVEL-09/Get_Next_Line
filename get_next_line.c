d60fbdb09c66b25cacb30d11dbb0db77568778ab
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-12-09 12:25:30 +0100

    fix redefine buffersize and return buf

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,19 @@
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 09:20:26 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/09 10:58:35 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/12/09 11:52:45 by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	char	*buf;
 
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
+	return (buf);
 }
