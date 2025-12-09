e708c3e5e3dd97d9ae59f3b3fbb80c0f7b34a6dc
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-12-09 13:02:40 +0100

    i have sigfult :(

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,28 @@
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 09:20:26 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/09 11:52:45 by mkhoubaz         ###   ########.fr       */
+/*   Updated: 2025/12/09 12:56:40 mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#include <stdio.h>
 char	*get_next_line(int fd)
 {
-	char	*buf;
+	static size_t	i;
+	char			*buf;
 
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	read(fd, buf, BUFFER_SIZE);
+	while (1)
+	{
+		read(fd, buf, i+1);
+		printf("%zu \n", i);
+		if (buf[i++] == '\n')
+			break;
+	}
+	
 	return (buf);
 }
