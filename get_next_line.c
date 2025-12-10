5ac4c62713bbbf10dcb53cd1b2a50eb3a716a842
Mohamed Khoubaz <hichamohicham651@gmail.com>
2025-12-10 10:30:33 +0100

    :>

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkhoubaz <mkhoubaz@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/08 09:20:26 by mkhoubaz          #+#    #+#             */
-/*   Updated: 2025/12/09 12:56:40 mkhoubaz         ###   ########.fr       */
+/*   Created: 2025/12/10 08:37:25 by mkhoubaz          #+#    #+#             */
+/*   Updated: 2025/12/10 09:09:13by mkhoubaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,28 @@ static void	*ft_memcpy(void *dest, const void *src, size_t n)
 char	*get_next_line(int fd)
 {
 	static size_t	i;
-	char			*buf;
-	char 			*line;
+	static int		x;
+	static int		z;
+	static char		*buf;
+	char			*line;
 
-	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
-		return (NULL);
-	read(fd, buf, BUFFER_SIZE);
+	if (!x)
+	{
+		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buf)
+			return (NULL);
+		read(fd, buf, BUFFER_SIZE);
+		x = 1;
+	}
 	while (i < BUFFER_SIZE)
 	{
-		if (buf[i++] == '\n')
-			break;
+		if (buf[i++] == '\n' || buf[i] == '\0')
+			break ;
 	}
-
-	line = malloc(sizeof(char) * i);
+	line = malloc(sizeof(char) * (i-z));
 	if (!line)
 		return (line);
-	ft_memcpy(line, buf, i);
+	ft_memcpy(line, buf+z, i-z);
+	z = i;
 	return (line);
 }
